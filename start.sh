75cf8b7d11e203de309d9e2604f74a5c87db4f66
Christoph Held <c-held@web.de>
2018-08-16 15:31:26 +0200

    write remote commands to disk

--- start.sh
+++ start.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 cd $(dirname ${BASH_SOURCE})
 
+# Environment
+source config/environment.cfg
+echo "------------------------------------------------"
+echo "The following environment configuration is used"
+echo "Public hostname: $PUBLIC_HOST_NAME"
+echo "Host IP: $HOST_IP"
+echo "Update config/environment.cfg if needed"
+echo "------------------------------------------------"
+echo 
+echo
+sleep 1
+
 # Configuration
 CATALOG_PATH=https://raw.githubusercontent.com/cheld/k8s-demo/master/config/catalog-aws.json
 VERSION_OPENSHIFT=openshift-origin-client-tools-v3.9.0-191fece-linux-64bit
 VERSION_ISTIO=istio-1.0.0
-HOST_IP=$(/sbin/ip route | awk '/default/ { print $3 }')
+
 
 # Shortcuts
 OC=bin/$VERSION_OPENSHIFT/oc
@@ -41,7 +53,7 @@ fi
 
 
 # Deploy Openshift
-$OC cluster up --skip-registry-check=true --public-hostname=$HOST_IP #--service-catalog
+$OC cluster up --skip-registry-check=true --public-hostname=$PUBLIC_HOST_NAME #--service-catalog
 $OC login -u system:admin
 
 # Deploy Broker Demo
@@ -80,54 +92,70 @@ wait_for_pod grafana
 #wait_for_pod kibana
 
 
+# General settings for test playground
+$OC adm policy add-scc-to-user anyuid -z default -n myproject
+$OC adm policy add-scc-to-user privileged -z default -n myproject
+oc adm policy add-cluster-role-to-user cluster-admin admin
+
+
 # Deploy sample application
-#$OC adm policy add-scc-to-user anyuid -z default -n myproject
-#$OC adm policy add-scc-to-user privileged -z default -n myproject
 #$OC apply -f <($ISTIOCTL kube-inject -f $DIR_ISTIO/samples/bookinfo/platform/kube/bookinfo.yaml)                                           
 #$OC create -f $DIR_ISTIO/samples/bookinfo/networking/bookinfo-gateway.yaml
 #wait_for_pod productpage
 #wait_for_pod ratings
 #wait_for_pod reviews
 
-# Set variables
-INGRESS_HOST=$HOST_IP
-INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].nodePort}')
-SECURE_INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="https")].nodePort}')
-
-
 # Output for easy usage
 echo 
 echo
-echo "-------------------------Bin----------------------------"
+echo "-------------------------Make binaries available on path----------------------------"
 echo "sudo ln -sfn `pwd`/$OC /usr/local/bin/oc"
 echo "sudo ln -sfn `pwd`/$ISTIOCTL /usr/local/bin/istioctl"
 echo
-echo "-----------------------Tools----------------------------"
-echo "Prometeus"
-echo "oc -n istio-system port-forward $(oc -n istio-system get pod -l app=prometheus -o jsonpath='{.items[0].metadata.name}') 9090:9090 &"
-echo "http://localhost:9090/graph#%5B%7B%22range_input%22%3A%221h%22%2C%22expr%22%3A%22istio_double_request_count%22%2C%22tab%22%3A1%7D%5D"
-echo "Jaeger"
-echo "oc port-forward -n istio-system $(oc get pod -n istio-system -l app=jaeger -o jsonpath='{.items[0].metadata.name}') 16686:16686 &"
-echo "http://localhost:16686"
-echo "Grafana"
-echo "oc -n istio-system port-forward $(oc -n istio-system get pod -l app=grafana -o jsonpath='{.items[0].metadata.name}') 3000:3000 &"
-echo "http://localhost:3000/dashboard/db/istio-mesh-dashboard"
-echo "Kibana"
-echo "oc -n logging port-forward \$(oc -n logging get pod -l app=kibana -o jsonpath='{.items[0].metadata.name}') 5601:5601 &"
-echo "http://localhost:5601/"
 echo
-echo "----------------------Variables-------------------------"
-echo "export INGRESS_HOST=$INGRESS_HOST"
+echo "----------------------Variables-----------------------------------------------------"
+INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].nodePort}')
+SECURE_INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="https")].nodePort}')
+echo "export INGRESS_HOST=$HOST_IP"
 echo "export INGRESS_PORT=$INGRESS_PORT"
 echo "export SECURE_INGRESS_PORT=$SECURE_INGRESS_PORT"
 echo
 echo
-echo "-----------------------Links----------------------------"
-echo "Openshift at https://$HOST_IP:8443"
-echo "Example at http://$HOST_IP:$INGRESS_PORT/productpage"
+echo "-----------------------Links--------------------------------------------------------"
+echo "Openshift at https://$PUBLIC_HOST_NAME:8443"
+echo "Example at http://$PUBLIC_HOST_NAME:$INGRESS_PORT/productpage"
 echo
 echo
+
+cat <<EOF >remote-access.txt
+-------------------------Remote Access----------------------------------------------
+Download oc from https://github.com/openshift/origin/releases
+oc login $HOST_IP -u admin
+
+-------------------------Tools------------------------------------------------------
+(Please note: all commands are executed on local client machine - not Kubernetes server)
+
+Prometeus
+oc -n istio-system port-forward $(oc -n istio-system get pod -l app=prometheus -o jsonpath='{.items[0].metadata.name}') 9090:9090 &
+Open browser: http://localhost:9090/graph#%5B%7B%22range_input%22%3A%221h%22%2C%22expr%22%3A%22istio_double_request_count%22%2C%22tab%22%3A1%7D%5D
+
+Jaeger
+oc port-forward -n istio-system $(oc get pod -n istio-system -l app=jaeger -o jsonpath='{.items[0].metadata.name}') 16686:16686 &
+Open browser: http://localhost:16686
+
+Grafana
+oc -n istio-system port-forward $(oc -n istio-system get pod -l app=grafana -o jsonpath='{.items[0].metadata.name}') 3000:3000 &
+Open browser: http://localhost:3000/dashboard/db/istio-mesh-dashboard
+EOF
+
+echo "---------------------------Remote Access-----------------------------------"
+echo "'cat remote-access.txt' for details"
 echo
+
+
+#Kibana
+#oc -n logging port-forward \$(oc -n logging get pod -l app=kibana -o jsonpath='{.items[0].metadata.name}') 5601:5601 &"
+#echo "http://localhost:5601/"
 
 # Free disk space
 # du -s -m -x -h * | sort -n
